046f0991dc4c53868fe308a72e7616d27d57bbd3
SerhiiCho <serhiicho@protonmail.com>
2022-06-22 12:29:48 +0300

    Added navbar for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar: React.FC = () => {
+    const [isOpened, setIsOpened] = useState<boolean>(false)
+
     const links = [
         { title: 'About', link: '#about' },
         { title: 'Professional skill', link: '#skill' },
@@ -9,12 +11,27 @@ const Navbar: React.FC = () => {
         { title: 'Contact', link: '#contact' },
     ]
 
+    function showNavbarHandler(): void {
+        setIsOpened(!isOpened)
+    }
+
     return (
-        <nav className="pt-12 text-white relative z-20">
-            <div className="hidden md:block">
-                <ul className="flex justify-end space-x-6 uppercase text-sm">
+        <nav className="pt-7 lg:pt-12 text-w">
+            <div className="flex md:hidden justify-end">
+                <button type="button"
+                    data-toggle="collapse"
+                    className="flex flex-col gap-y-[6px] py-2 px-4 drop-shadow-font"
+                    onClick={showNavbarHandler}
+                >
+                    <div className="bg-white h-[3px] w-6 rounded-full"></div>
+                    <div className="bg-white h-[3px] w-6 rounded-full"></div>
+                </button>
+            </div>
+
+            <div className={`${isOpened ? 'h-60 border-t' : 'h-0'} md:static md:h-full overflow-hidden pt-6 md:pt-0 mt-6 md:mt-0 border-white/20 transition-height duration-500`}>
+                <ul className="flex flex-col md:flex-row justify-end space-y-3 md:space-y-0 md:space-x-6 uppercase text-md lg:text-sm">
                     {links.map(l => {
-                        return <li>
+                        return <li key={l.title}>
                             <button
                                 type="button"
                                 data-target={l.link}
