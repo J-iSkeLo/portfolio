0df0c7196ccf3228220e03b68b05865adfa71516
SerhiiCho <serhiicho@protonmail.com>
2022-06-22 21:57:35 +0300

    Added init for smooth loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from 'react'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Skills from './sections/Skills'
 import Personal from './sections/Personal'
 import { ParallaxProvider } from 'react-scroll-parallax';
+import smoothLoader from 'smooth-loader'
 
 const App = () => {
+    useEffect(() => {
+        smoothLoader('img[data-src]')
+    }, [])
+
     return (
         <ParallaxProvider>
             <div className="text-gray-700">
