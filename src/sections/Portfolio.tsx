0bb531de3db6643978dbf6a5ae70791df31c83ba
SerhiiCho <serhiicho@protonmail.com>
2022-06-23 10:37:26 +0300

    WIP on portfolio section

--- src/sections/Portfolio.tsx
+++ src/sections/Portfolio.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import Section from '../../components/Section/Section'
-import products from '../../modules/products'
-import Product from './Product'
-import { Product as ProductType } from '../../types/types'
+import Section from '../components/Section/Section'
+import products from '../modules/products'
+import { Product as ProductType } from '../types/types'
 
 const Portfolio: React.FC = () => {
     const [selectedProduct, setSelectedProduct] = useState<ProductType>(products[0])
@@ -16,7 +15,7 @@ const Portfolio: React.FC = () => {
                     <div className="flex justify-evenly flex-wrap gap-5">
                         {products.map(product => {
                             return (
-                                <button className={(product.id === selectedProduct.id ? 'border-main from-secondary to-main text-white' : 'from-gray-100 via-gray-50 to-white') +  ` bg-gradient bg-gradient-to-br py-3 px-10 rounded-full border border-gray-100 shadow-lg transition-shadow hover:shadow-xl duration-500`}>
+                                <button key={product.id} className={(product.id === selectedProduct.id ? 'border-main from-secondary to-main text-white' : 'from-gray-100 via-gray-50 to-white') +  ` bg-gradient bg-gradient-to-br py-3 px-10 rounded-full border border-gray-100 shadow-lg transition-shadow hover:shadow-xl duration-500`}>
                                     <span className="drop-shadow-md">{product.title}</span>
                                 </button>
                             )
@@ -29,8 +28,14 @@ const Portfolio: React.FC = () => {
                     </p>
 
                     {/* Items */}
-                    <div>
-                        {products.map(product => <Product key={product.id} product={product} />)}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+                        {selectedProduct.items.map(item => {
+                            return (
+                                <div key={item}>
+                                    <img src={item} alt={selectedProduct.title} className="max-w-auto md:max-w-[220px] mx-auto" />
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </Section>
