9c46b7dd2b85c295b69b29078d6385158cf368b9
SerhiiCho <serhiicho@protonmail.com>
2022-06-23 13:49:52 +0300

    Done with Reviews section

--- src/sections/Reviews.tsx
+++ src/sections/Reviews.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Section from '../components/Section/Section'
 import reviews from '../modules/reviews'
+import bgImage from '../assets/img/backgrounds/reviews.png'
 
 const Reviews: React.FC = () => {
     return (
         <div>
-            <Section title="Reviews">
+            <Section title="Reviews" layers={[{ image: bgImage, speed: -20 }]}>
                 <div className="container">
-                    <ul className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                         {reviews.map(review => {
                             return (
                                 <li key={review.name}>
-                                    <figure className="relative flex flex-col rounded-lg p-6 shadow-lg border border-gray-50">
+                                    <figure className="relative flex flex-col rounded-lg p-6 shadow-lg border border-gray-50 bg-gradient bg-gradient-to-br from-gray-100 via-white to-gray-200">
                                         <figcaption className="flex items-center space-x-4">
                                             <img data-src={review.img}
                                                 alt={review.companyName}
