a2bee53ffeec141f4c886fa708133f5e73834404
SerhiiCho <serhiicho@protonmail.com>
2022-06-23 14:32:24 +0300

    WIP on contact me section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import Section from '../components/Section/Section'
+import contacts from '../modules/contactMe'
 
 const Contact: React.FC = () => {
     return (
         <div className="px-7">
             <Section title="Contact me">
                 <div className="container">
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                        
+                    <div className="flex flex-col md:flex-row align-center justify-center gap-y-8 gap-x-14">
+                        {contacts.map(contact => {
+                            return (
+                                <a href={contact.href}
+                                    key={contact.title}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="flex items-center gap-x-3 mx-auto md:mx-0"
+                                >
+                                    {contact.icon}
+                                    <span className="text-md">{contact.title}</span>
+                                </a>
+                            )
+                        })}
                     </div>
                 </div>
             </Section>
