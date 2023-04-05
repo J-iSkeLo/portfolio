157a772c3f822fbe7ab110c65c62697626104af3
SerhiiCho <serhiicho@protonmail.com>
2023-04-05 11:40:24 +0300

    Refactored Reviews.tsx

---
+++ src/components/Review.tsx
@@ -0,0 +1,32 @@
+import type { Review as ReviewType } from '@/types/types'
+import React from 'react'
+
+interface Props {
+    review: ReviewType
+}
+
+const Review: React.FC<Props> = ({ review }) => {
+    return (
+        <li key={review.name}>
+            <figure className="relative flex flex-col rounded-lg p-6 shadow-lg border border-gray-50 bg-gradient bg-gradient-to-br from-gray-100 via-white to-gray-200">
+                <figcaption className="flex items-center space-x-4">
+                    <img src={review.img}
+                        alt={review.companyName}
+                        className="h-14 w-14 rounded-full shadow-md"
+                    />
+
+                    <div>
+                        <h3 className="font-bold text-xl text-gray-600">{review.name}</h3>
+                        <h3 className="text-gray-400">{review.companyName}</h3>
+                    </div>
+                </figcaption>
+
+                <blockquote className="mt-5 text-gray-500 leading-6">
+                    <p>{review.review}</p>
+                </blockquote>
+            </figure>
+        </li>
+    )
+}
+
+export default Review
--- src/sections/Reviews.tsx
+++ src/sections/Reviews.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Section from '@/components/Section/Section'
 import reviews from '@/modules/reviews'
 import bgImage from '@/assets/img/backgrounds/reviews.webp'
+import Review from '@/components/Review'
 
 const Reviews: React.FC = () => {
     return (
@@ -9,29 +10,7 @@ const Reviews: React.FC = () => {
             <Section title="Reviews" layers={[{ image: bgImage, speed: -20 }]}>
                 <div className="container">
                     <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                        {reviews.map(review => {
-                            return (
-                                <li key={review.name}>
-                                    <figure className="relative flex flex-col rounded-lg p-6 shadow-lg border border-gray-50 bg-gradient bg-gradient-to-br from-gray-100 via-white to-gray-200">
-                                        <figcaption className="flex items-center space-x-4">
-                                            <img src={review.img}
-                                                alt={review.companyName}
-                                                className="h-14 w-14 rounded-full shadow-md"
-                                            />
-
-                                            <div>
-                                                <h3 className="font-bold text-xl text-gray-600">{review.name}</h3>
-                                                <h3 className="text-gray-400">{review.companyName}</h3>
-                                            </div>
-                                        </figcaption>
-
-                                        <blockquote className="mt-5 text-gray-500 leading-6">
-                                            <p>{review.review}</p>
-                                        </blockquote>
-                                    </figure>
-                                </li>
-                            )
-                        })}
+                        {reviews.map(r => <Review review={r} />)}
                     </ul>
                 </div>
             </Section>
