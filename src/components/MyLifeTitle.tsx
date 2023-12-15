1654dae4165bebd34dc90b36ea3d62c27a229e0a
SerhiiCho <serhiicho@protonmail.com>
2023-12-15 08:58:37 +0200

    Refactor MyLifeTitle component styling

--- src/components/MyLifeTitle.tsx
+++ src/components/MyLifeTitle.tsx
@@ -33,14 +33,14 @@ const MyLifeTitle: React.FC<Props> = (props) => {
     return <>
         <div
             onClick={handleTitleClick}
-            className="flex items-center justify-between gap-3 leading-6 text-sm md:text-md xl:text-xl border border-white/20 px-4 py-2 rounded-lg cursor-pointer hover:bg-black/10 transition-colors"
+            className={`flex items-center justify-between gap-3 leading-6 text-sm md:text-md xl:text-xl border px-4 py-2 rounded-lg cursor-pointer transition-colors ${showContent ? 'border-purple-300/50 text-purple-200 bg-black/20' : 'border-white/20 hover:bg-black/10'}`}
         >
             <h2>{props.header}</h2>
             <span>{showContent ? <MinusICon /> : <PlusIcon />}</span>
         </div>
 
         <div className={`mt-4 ${showContent ? 'block' : 'hidden'}`}>
-            <div className="px-3 py-1 text-[.95em] lg:text-md">
+            <div className="p-4 text-[.95em] lg:text-md bg-black/20 rounded-xl">
                 {props.children}
             </div>
         </div>
