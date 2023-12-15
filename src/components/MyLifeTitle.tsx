0762cd6c357a92c33cb736d8a4d09d0aaaf6d7c2
SerhiiCho <serhiicho@protonmail.com>
2023-12-15 08:49:38 +0200

    Add closing title content when other is opened

--- src/components/MyLifeTitle.tsx
+++ src/components/MyLifeTitle.tsx
@@ -1,7 +1,10 @@
 import type React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PlusIcon from '@/components/Icons/PlusIcon'
 import MinusICon from '@/components/Icons/MinusIcon'
+import { events } from 'config'
+import dispatchEvent from '@/modules/dispatchEvent'
+import listenEvent from '@/modules/listenEvent'
 
 interface Props {
     children: React.ReactNode
@@ -11,9 +14,25 @@ interface Props {
 const MyLifeTitle: React.FC<Props> = (props) => {
     const [showContent, setShowContent] = useState(false)
 
+    useEffect(() => {
+        listenEvent<string>(events.myLifeTitleOpened, header => {
+            if (header !== props.header) {
+                setShowContent(false)
+            }
+        })
+    }, [])
+
+    function handleTitleClick() {
+        if (showContent === false) {
+            dispatchEvent<string>(events.myLifeTitleOpened, props.header)
+        }
+
+        setShowContent(!showContent)
+    }
+
     return <>
         <div
-            onClick={() => setShowContent(!showContent)}
+            onClick={handleTitleClick}
             className="flex items-center justify-between gap-3 leading-6 text-sm md:text-md xl:text-xl border border-white/20 px-4 py-2 rounded-lg cursor-pointer hover:bg-black/10 transition-colors"
         >
             <h2>{props.header}</h2>
