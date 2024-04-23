c768f744728a190a1fea6cbddd3383b5340e75b0
SerhiiCho <serhiicho@protonmail.com>
2024-04-23 14:19:22 +0300

    Refactor Portfolio

---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { Product } from '@/types/types'
+
+type Props = {
+    product: Product
+    isSelected: boolean
+    img: string
+}
+
+const ProductItem: React.FC<Props> = ({ product, isSelected, img }) => {
+    const zoomImageWrapClasses = 'fixed cursor-zoom-out top-0 left-0 w-full h-full bg-black bg-opacity-75 z-50 flex items-center justify-center'
+    const zoomImageClasses = 'max-w-none h-[calc(100%-100px)] w-auto'
+
+    const [zoomedImage, setZoomedImage] = useState<string | null>(null)
+
+    function setZoomedImageHandler(item: string): void {
+        if (zoomedImage === item) {
+            setZoomedImage(null)
+            return
+        }
+
+        setZoomedImage(item)
+    }
+
+    return (
+        <div
+            key={img}
+            className={
+                (isSelected ? '' : 'hidden')
+                + ' ' +
+                (zoomedImage === img ? zoomImageWrapClasses : 'cursor-zoom-in')
+                + ' transition-all duration-300 hover:scale-105'
+            }
+            onClick={() => setZoomedImageHandler(img)}
+        >
+            <img
+                data-src={img}
+                width="220"
+                height="468"
+                alt={product.title}
+                className={
+                    'transition-all duration-300 mx-auto drop-shadow-md'
+                    + ' ' +
+                    (zoomedImage === img ? zoomImageClasses : 'max-w-auto md:max-w-[220px]')
+                }
+            />
+        </div>
+    )
+}
+
+export default ProductItem
--- src/components/sections/Portfolio.tsx
+++ src/components/sections/Portfolio.tsx
@@ -2,21 +2,10 @@ import React, { useState } from 'react'
 import Section from '@/components/Section/Section'
 import products from '@/modules/products'
 import { Product as ProductType } from '@/types/types'
+import ProductItem from '@/components/ProductItem'
 
 const Portfolio: React.FC = () => {
     const [selectedProduct, setSelectedProduct] = useState<ProductType>(products[0])
-    const [zoomedImage, setZoomedImage] = useState<string | null>(null)
-    const zoomImageWrapClasses = 'fixed cursor-zoom-out top-0 left-0 w-full h-full bg-black bg-opacity-75 z-50 flex items-center justify-center'
-    const zoomImageClasses = 'max-w-none h-[calc(100%-100px)] w-auto'
-
-    function setZoomedImageHandler(item: string): void {
-        if (zoomedImage === item) {
-            setZoomedImage(null)
-            return
-        }
-
-        setZoomedImage(item)
-    }
 
     return (
         <div className="lg:px-7" id="portfolio">
@@ -46,30 +35,13 @@ const Portfolio: React.FC = () => {
                     {/* Items */}
                     <div className="min-h-[450px] grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 lg:gap-6">
                         {products.map(product => {
-                            return product.items.map(item => {
+                            return product.items.map(img => {
                                 return (
-                                    <div
-                                        key={item}
-                                        className={
-                                            (selectedProduct.id === product.id ? '' : 'hidden')
-                                            + ' ' +
-                                            (zoomedImage === item ? zoomImageWrapClasses : 'cursor-zoom-in')
-                                            + ' transition-all duration-300 hover:scale-105'
-                                        }
-                                        onClick={() => setZoomedImageHandler(item)}
-                                    >
-                                        <img
-                                            data-src={item}
-                                            width="220"
-                                            height="468"
-                                            alt={product.title}
-                                            className={
-                                                'transition-all duration-300 mx-auto drop-shadow-md'
-                                                + ' ' +
-                                                (zoomedImage === item ? zoomImageClasses : 'max-w-auto md:max-w-[220px]')
-                                            }
-                                        />
-                                    </div>
+                                    <ProductItem
+                                        product={product}
+                                        isSelected={selectedProduct.id === product.id}
+                                        img={img}
+                                    />
                                 )
                             })
                         })}
