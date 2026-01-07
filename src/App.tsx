14b897403efe92eb9084b532a45d2cdef93acb1e
SerhiiCho <serhiicho@protonmail.com>
2026-01-07 17:44:33 +0200

    chore: fix

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App = () => {
                 <Portfolio />
                 <Testimonials />
                 <Contact />
-                <Author />
+                {/* <Author /> */}
                 <ToTop />
             </div>
         </ParallaxProvider>
