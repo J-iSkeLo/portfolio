dbd270258e5fc3ab0055adc4be0669db8a022370
SerhiiCho <serhiicho@protonmail.com>
2026-01-09 14:35:49 +0200

    chore: remove not used code

--- src/modules/dispatchEvent.ts
+++
@@ -1,6 +0,0 @@
-const dispatchEvent = <T>(name: string, params?: T) => {
-    const event = new CustomEvent(name, { detail: params })
-    window.dispatchEvent(event)
-}
-
-export default dispatchEvent
--- src/modules/listenEvent.ts
+++
@@ -1,7 +0,0 @@
-const listenEvent = <T>(name: string, callback: (data: T) => void) => {
-    const handler = (e: any) => callback(e.detail)
-    window.addEventListener(name, handler)
-    return () => window.removeEventListener(name, handler)
-}
-
-export default listenEvent
