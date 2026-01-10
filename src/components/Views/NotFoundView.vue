786c18e7bd51dd6a3e30348be24714f87d598867
SerhiiCho <serhiicho@protonmail.com>
2026-01-10 11:26:45 +0200

    fix: add 404 page

---
+++ src/components/Views/NotFoundView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Hero from '@/components/Sections/Hero.vue'
+</script>
+
+<template>
+    <Hero :is-home="false" />
+    <div class="pt-20 container h-screen flex flex-col items-center">
+        <h2 class="text-3xl font-bold">404. Not found</h2>
+        <h3 class="text-lg mt-2">The page you are looking for is not found</h3>
+
+        <RouterLink
+            :to="{ name: 'home' }"
+            :class="[
+                'bg-secondary text-white px-7 py-2 mt-6 rounded-full',
+                'transition-shadow text-lg shadow-md hover:shadow-lg',
+            ]"
+        >
+            Take me home
+        </RouterLink>
+    </div>
+</template>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,9 @@
 import HomeView from '@/components/Views/HomeView.vue'
 import LifeView from '@/components/Views/LifeView.vue'
+import NotFoundView from '@/components/Views/NotFoundView.vue'
 
 export const routes = [
     { path: '/', name: 'home', component: HomeView },
     { path: '/life', name: 'life', component: LifeView },
+    { path: '/:catchAll(.*)', component: NotFoundView },
 ]
